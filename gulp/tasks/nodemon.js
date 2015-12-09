7e0fbbaa5958a6d5a4168f5a3271fb1f8001ceda
Omri Litov <omrilitov@gmail.com>
2015-12-09 16:20:00 +0200

    Added babel nodemon support

--- gulp/tasks/nodemon.js
+++ gulp/tasks/nodemon.js
@@ -1,11 +1,14 @@
 'use strict';
 
+import {resolve} from 'path';
 import config from '../config';
-import nodemon from 'nodemon';
+import nodemon from 'gulp-nodemon';
 
 export default gulp => {
-  gulp.task('nodemon', cb => {
-    nodemon({
+  gulp.task('nodemon', () => {
+    return nodemon({
+      exec: resolve('./node_modules/.bin/babel-node'),
+
       script: 'server/app.js',
 
       options: {
@@ -14,7 +17,6 @@ export default gulp => {
 
       // Watch core server file(s) that require server restart on change
       watch: [config.paths.server]
-    })
-      .once('start', cb);
+    });
   });
 };
