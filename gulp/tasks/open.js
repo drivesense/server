beaf6569283dd4bd103da6a0539fc8303f59cd82
Noam Okman <noamokman@gmail.com>
2016-03-09 18:45:39 +0200

    chore(npm-scripts): removed gulp

--- gulp/tasks/open.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import open from 'open';
-
-export default gulp => {
-  gulp.task('open', () => {
-    open(`${process.env.USE_SSL ? 'https' : 'http'}://localhost:${process.env.PORT}`);
-  });
-};
