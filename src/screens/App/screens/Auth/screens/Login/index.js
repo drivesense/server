38adabb11390f77089adc59985833d1a58ffd95c
EladBezalel <Elad.bezalel@gmail.com>
2016-03-11 14:21:01 +0200

    refactor(login): moved all styles inside an object

--- src/screens/App/screens/Auth/screens/Login/index.js
+++ src/screens/App/screens/Auth/screens/Login/index.js
@@ -8,42 +8,44 @@ import { Card,
          FlatButton,
          TextField } from 'material-ui';
 
-const container = {
-  flex: 1,
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center',
-  boxSizing: 'border-box',
-  display: 'flex',
+const styles = {
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    boxSizing: 'border-box',
+    display: 'flex',
 
-  width: '100%',
-  height: '100%',
-  minHeight: '100%',
+    width: '100%',
+    height: '100%',
+    minHeight: '100%',
 
-  backgroundColor: Colors.indigo500
-};
-
-const cardActions = {
-  display: 'flex',
-  justifyContent: 'flex-end'
-};
-
-const cardText = {
-  display: 'flex',
-  flexDirection: 'column'
+    backgroundColor: Colors.indigo500
+  },
+  card: {
+    actions: {
+      display: 'flex',
+      justifyContent: 'flex-end'
+    },
+    text: {
+      display: 'flex',
+      flexDirection: 'column'
+    }
+  }
 };
 
 export default class Login extends React.Component {
   render() {
     return (
-      <div style={container}>
+      <div style={styles.container}>
         <Card>
           <CardTitle title="Login"></CardTitle>
-          <CardText style={cardText}>
+          <CardText style={styles.card.text}>
             <TextField floatingLabelText="Email"></TextField>
             <TextField floatingLabelText="Password" type="password"></TextField>
           </CardText>
-          <CardActions style={cardActions}>
+          <CardActions style={styles.card.actions}>
             <FlatButton label="Not a user?"></FlatButton>
             <FlatButton label="Let's go!" secondary={true}></FlatButton>
           </CardActions>
