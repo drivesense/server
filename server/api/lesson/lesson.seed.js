70e55f02bfc2c322f04dc76b9f51fc941160551e
Omri Litov <omrilitov@gmail.com>
2016-04-24 16:19:44 +0300

    a

--- server/api/lesson/lesson.seed.js
+++ server/api/lesson/lesson.seed.js
@@ -5,9 +5,13 @@ import _ from 'lodash';
 
 export default {
   dependencies: [User],
-  seed: users => _.times(10, i => ({
-    student: users[5],
-    teacher: users[4],
-    date: Date.now() + i * 3 * 1000 * 60 * 60 * 24
+  seed: users => _.flatten(_.times(2, j => {
+    return _.flatten(_.times(3, s => {
+      return _.times(10, i => ({
+        student: users[users.length - 1 - (j * 4 + s)],
+        teacher: users[users.length - ((j + 1) * 4)],
+        date: Date.now() + i * (3 + s) * 1000 * 60 * 60 * 24
+      }))
+    }));
   }))
 };
