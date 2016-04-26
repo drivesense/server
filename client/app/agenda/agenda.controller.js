bda7f9f55add272627721249b88d0fd541a5cb87
Omri Litov <omrilitov@gmail.com>
2016-04-26 18:02:54 +0300

    and

--- client/app/agenda/agenda.controller.js
+++ client/app/agenda/agenda.controller.js
@@ -3,33 +3,40 @@
 angular.module('drivesenseApp')
   .controller('Agenda', function ($scope, Auth, lessons, moment) {
     $scope.userType = Auth.getCurrentUser().type;
-    var groupBy = function (lessons, sheker) {
-      return _.groupBy(lessons, function(lesson) {
-        return moment(lesson.date).startOf(sheker);
+    var groupBy = function (lessonsToGroup, by) {
+      return _.groupBy(lessonsToGroup, function(lesson) {
+        return moment(lesson.date).startOf(by);
       })
     };
 
     var dayLessons = groupBy(lessons, 'day');
-    var hourLessons = _.mapValues(dayLessons, function (lessons) {
-      return _.groupBy(lessons, 'hour')
-    });
+    $scope.newLessons = _.mapValues(dayLessons, function (lessons) {
+      var byHours = groupBy(lessons, 'hour');
 
-    $scope.lessons = _.mapValues(hourLessons, function (lessons) {
-      return _.groupBy(lessons, function(lesson) {
-        return moment(lesson.date);
+      byHours =_.mapValues(byHours, function (temp2) {
+        return _.groupBy(temp2, function(lesson) {
+          return moment(lesson.date);
+        });
       });
+
+      return byHours;
     });
-
-
-    console.log($scope.lessons);
 
     $scope.days = _.times(7, function(i) {
       return moment().startOf('day').add(-1 + i, 'day');
     });
 
-    $scope.hours = _.times(16, function (i) {
-      return moment().startOf('day').add(6 + i, 'hour');
-    });
+    $scope.makeHours = function (day) {
+      return _.times(16, function (i) {
+        return moment(day).add(6 + i, 'hour');
+      });
+    };
+
+    $scope.makeQuarters = function (hour) {
+      return _.times(4, function (i) {
+        return moment(hour).add(15 * i, 'minutes');
+      });
+    };
 
     $scope.getDay = function (day) {
       return day.calendar(null, {
