a35af7d4b234ed6794fa76e438e9d4d49764a075
Noam Okman <noamokman@gmail.com>
2016-06-08 14:01:09 +0300

    random-time madness

--- server/components/alg/greedy/index.js
+++ server/components/alg/greedy/index.js
@@ -2,11 +2,19 @@ import _ from 'lodash';
 import {getMatches} from '../util/match';
 import {iterateDay} from '../util/iterate';
 
+const randomFactor = 0.3;
+
 const getCombo = (date, students, teacher, normalize) => {
   const combs = getMatches(date, students, teacher, normalize);
 
   if (!combs.length) {
     return null;
+  }
+
+  if (Math.random() < randomFactor) {
+    const combos = _.sortBy(combs, 'grade');
+
+    return _.takeRight(combos, 2)[0];
   }
 
   return _.maxBy(combs, 'grade');
